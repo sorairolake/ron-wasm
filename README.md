37418ced191724e166722556d49ec1fca3ed708e
Shun Sakai <sorairolake@protonmail.ch>
2024-06-13 16:19:37 +0900

    Initialize Cargo package

--- README.md
+++ README.md
@@ -46,7 +46,7 @@ See the [documentation][docs-url] for more details.
 
 ## Minimum supported Rust version
 
-The minimum supported Rust version (MSRV) of this library is v1.74.0.
+The minimum supported Rust version (MSRV) of this library is v1.64.0.
 
 ## Changelog
 
---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+// SPDX-FileCopyrightText: 2024 Shun Sakai
+//
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+//! The `ron-wasm` crate is the Wasm bindings for [RON] (Rusty Object Notation).
+//!
+//! [RON]: https://github.com/ron-rs/ron
+
+#![doc(html_root_url = "https://docs.rs/ron-wasm/0.1.0/")]
+// Lint levels of rustc.
+#![forbid(unsafe_code)]
+#![deny(missing_debug_implementations, missing_docs)]
+#![warn(rust_2018_idioms)]
+// Lint levels of Clippy.
+#![warn(clippy::cargo, clippy::nursery, clippy::pedantic)]
+
+mod parse;
+mod stringify;
